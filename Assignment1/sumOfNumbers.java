ad73cd5520f6d44b9590619970cb61f58f322de8
AgnishPOC <agnishshyam@outlook.com>
2016-10-19 12:02:06 +0530

    Add files via upload

---
+++ Assignment1/sumOfNumbers.java
@@ -0,0 +1,12 @@
+package Day1.Assignment1;
+
+public class sumOfNumbers {
+
+	public static void main(String[] args) {
+		int x = 5;
+		float y = 5.7f;
+		int sum = x + (int) y;
+		System.out.println("Sum of Two Numbers[INT]=" + sum);
+	}
+
+}
